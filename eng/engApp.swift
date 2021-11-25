348285c966244a7c832b79d985da1ae4ffc79a87
Amir Diafi <amirdiafi2@gmail.com>
2021-11-25 15:00:39 +0100

    Initial Commit

---
+++ eng/engApp.swift
@@ -0,0 +1,17 @@
+//
+//  engApp.swift
+//  eng
+//
+//  Created by Mac on 11/25/21.
+//
+
+import SwiftUI
+
+@main
+struct engApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
