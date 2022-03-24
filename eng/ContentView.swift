f87787e8439b645249f7c3fd69fb5a626bc39531
Amir Diafi <amirdiafi2@gmail.com>
2022-03-24 12:01:27 +0100

    add files first time

--- eng/ContentView.swift
+++ eng/ContentView.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    var me : String = "Amir"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(me)
     }
+    var footer : some View {
+        Image(<#T##name: String##String#>)
+    }
+    var nmr : Double = 5.55555555555555555
+    var myArray : [String:Any] = ["name": "Amir", "age": 25]
 }
 
 struct ContentView_Previews: PreviewProvider {
